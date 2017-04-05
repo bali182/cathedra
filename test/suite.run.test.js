3f1b0e1847cbd13127deecc9e4d5da822de47f71
Édes Balázs <edesbalazs@gmail.com>
2017-04-05 23:39:00 +0100

    first tests for suites

--- test/suite.run.test.js
+++ test/suite.run.test.js
@@ -3,6 +3,23 @@ import { time } from '../src/common'
 
 describe('running suites', () => {
   const roundRandom = () => Math.round(Math.random() * 10)
+  const sqrtRandom = () => Math.sqrt(Math.random() * 10)
+  const powRandom = () => Math.pow(Math.random() * 10, 2)
+
+  const assertBenchmark = (targetName, targetTime) => ({ name, isBenchmark, isSuite, operations, pureTime, fullTime, children }) => {
+    expect(name).toBe(targetName)
+
+    expect(isBenchmark).toBe(true)
+    expect(isSuite).toBeUndefined()
+    expect(children).toBeUndefined()
+
+    expect(typeof operations).toBe('number')
+    expect(typeof pureTime).toBe('number')
+    expect(typeof fullTime).toBe('number')
+
+    expect(pureTime).toBeLessThanOrEqual(targetTime)
+    expect(fullTime).toBeGreaterThanOrEqual(targetTime)
+  }
 
   it('should run suite with one benchmark', () => {
     const s = suite(roundRandom)({ until: time(100), name: 'random' })
@@ -10,6 +27,32 @@ describe('running suites', () => {
 
     expect(name).toBe('random')
     expect(isSuite).toBe(true)
-    expect(Array.isArray(children)).toBe(true)
+
+    const [roundRndResult] = children
+
+    assertBenchmark(roundRandom.name, 100)(roundRndResult)
+  })
+
+  it('should run suite with multiple benchmarks', () => {
+    const s = suite(
+      roundRandom,
+      sqrtRandom,
+      powRandom
+    )
+
+    const configuredSuite = s({ until: time(100), name: 'math' })
+
+    const { name, isSuite, isBenchmark, children } = configuredSuite()
+
+    expect(name).toBe('math')
+    expect(isSuite).toBe(true)
+    expect(isBenchmark).toBeUndefined()
+    expect(children.length).toBe(3)
+
+    const [roundRndResult, sqrtRndResult, powRndResult] = children
+
+    assertBenchmark(roundRandom.name, 100)(roundRndResult)
+    assertBenchmark(sqrtRandom.name, 100)(sqrtRndResult)
+    assertBenchmark(powRandom.name, 100)(powRndResult)
   })
 })
